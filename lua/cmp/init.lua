f0b5c5695feb91bb2d7401d9ca68d97fab773973
xzb <2598514867@qq.com>
2025-01-24 19:06:09 +0800

    delay insertenter

--- lua/cmp/init.lua
+++ lua/cmp/init.lua
@@ -367,7 +367,11 @@ local on_insert_enter = function()
   end
 end
 autocmd.subscribe({ 'CmdlineEnter' }, async.debounce_next_tick(on_insert_enter))
-autocmd.subscribe({ 'InsertEnter' }, async.debounce_next_tick_by_keymap(on_insert_enter))
+autocmd.subscribe({ 'InsertEnter' }, function()
+  vim.defer_fn(function()
+    async.debounce_next_tick_by_keymap(on_insert_enter)()
+  end, 50)
+end)
 
 -- async.throttle is needed for performance. The mapping `:<C-u>...<CR>` will fire `CmdlineChanged` for each character.
 local on_text_changed = function()
